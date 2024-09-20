2be1ab3b490185b6c54b44d10068332b8b174e63
nitrogenez <nitrogenez.dev@tuta.io>
2024-09-20 21:59:00 +0300

    feat: zig 0.13.0 update

--- build.zig
+++ build.zig
@@ -1,17 +1,25 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
 
-    const lib = b.addStaticLibrary("zig-json5", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    _ = b.addModule("lumix-json", .{ .root_source_file = b.path("src/root.zig") });
 
-    const main_tests = b.addTest("src/main_test.zig");
-    main_tests.setBuildMode(mode);
+    const lib = b.addStaticLibrary(.{
+        .name = "lumixjson",
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(lib);
 
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    const lib_tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_lib_tests = b.addRunArtifact(lib_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_tests.step);
 }
